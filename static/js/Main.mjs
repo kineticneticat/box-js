cb5c5d768a1c67ab7457fe28cf46db03c2fdfa65
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2023-02-24 20:56:49 +0000

    drag

--- static/js/Main.mjs
+++ static/js/Main.mjs
@@ -2,9 +2,11 @@ import {ValuesToCssCode} from '../js/Nodes.mjs'
 
 export let canvas = document.getElementById('canvas')
 export let ctx = canvas.getContext('2d')
-export let clickPos
+export let clickPos = []
+export let mousePos = []
+// document.getElementById('out').innerHTML = 'hey leo'
 canvas.addEventListener('mousedown', e => {
-	clickPos = [e.clientX-10, e.clientY-10]
+	clickPos = [e.clientX-8, e.clientY-8]
 	for (let i in boxes) {
 		i=parseInt(i)
 		boxes[i].check(clickPos, i)
@@ -13,6 +15,15 @@ canvas.addEventListener('mousedown', e => {
 }, true)
 canvas.addEventListener('mouseup', e => {
 	boxes.map(b => b.held = false)
+	reDraw()
+}, true)
+canvas.addEventListener('mousemove', e => {
+	mousePos = [e.clientX-8, e.clientY-8]
+	for (let box of boxes) {
+		if (box.held) {
+			box.coords = mousePos
+		}
+	}
 	reDraw()
 }, true)
 
@@ -54,6 +65,6 @@ window.onload = () => {
 function reDraw() {
 	ctx.fillStyle = '#555555'
 	ctx.fillRect(0, 0, canvas.width, canvas.height)
-	boxes.forEach(x => x.draw())
+	clickHistory.forEach(x => boxes[x].draw())
 
 }
